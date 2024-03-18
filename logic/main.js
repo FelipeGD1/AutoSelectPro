ef5e2307b7bfd6f14468a4800d21b2f2df30ca2a
Lauris <valentinasanchezarias1804@gmail.com>
2024-03-18 13:19:32 -0500

    chat

--- logic/main.js
+++ logic/main.js
@@ -1,7 +1,7 @@
 var botonMostrar = document.getElementById('logo-chat');
 var formulario = document.getElementById('chatbot-form');
 var chatbot = document.getElementById('container-chatbot');
-  
+
 botonMostrar.addEventListener('click', function () {
   if(formulario.style.display === 'none') {
     formulario.style.display = 'flex';
